47a9954f286db5189f497ce3e1e283446295579e
Manraj <manrajkhokhar1995@gmail.com>
2020-03-06 15:46:08 -0800

    remove console.logs

--- util/weatherbit/fetchForecast.js
+++ util/weatherbit/fetchForecast.js
@@ -5,7 +5,6 @@ module.exports = (coord) => {
     let request = new XMLHttpRequest();
 
     request.open('GET', ` https://api.weatherbit.io/v2.0/forecast/daily?key=0e739b00f5aa4e30a4e3e0c601e1b133&${coord}`, true);
-    console.log('hitting 2');
 
     request.onload = function () {
         let data = JSON.parse(this.response);
